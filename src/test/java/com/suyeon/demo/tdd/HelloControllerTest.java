cfdab683179f13e10241f2be4f96bb7cbd2d0483
suyeonJung <suyeonjungdev@gmail.com>
2022-04-12 20:27:04 +0900

    api 생성 + api 응답 테스트

---
+++ src/test/java/com/suyeon/demo/tdd/HelloControllerTest.java
@@ -0,0 +1,29 @@
+package com.suyeon.demo.tdd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void responseHello() throws Exception {
+        String hello = "hello";
+
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
